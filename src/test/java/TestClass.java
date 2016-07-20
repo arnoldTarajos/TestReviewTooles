647a570de710745c8b65d8c045f12258dce58cb1
atarajos <arnold.tarajos@smedix.com>
2016-07-20 15:37:22 +0300

    Add first commmit with 1 test

---
+++ src/test/java/TestClass.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestClass
+{
+
+  @Test
+  public void testEqual ()
+  {
+    assertEquals(Boolean.TRUE, Boolean.TRUE);
+  }
+
+}
